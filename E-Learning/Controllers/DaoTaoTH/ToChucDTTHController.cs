1dbae44c66dd268f15e01dc07308b93283dd4bcf
HPDQ\nguyenvotuannhan <nguyenvotuannhan@hoaphat.com.vn>
2025-02-27 11:04:58 +0700

    add form edit

--- E-Learning/Controllers/DaoTaoTH/ToChucDTTHController.cs
+++ E-Learning/Controllers/DaoTaoTH/ToChucDTTHController.cs
@@ -1,4 +1,5 @@
 ﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Wordprocessing;
 using E_Learning.Models;
 using E_Learning.ModelsDTTH;
 using E_Learning.Services;
@@ -81,8 +82,8 @@ namespace E_Learning.Controllers.DaoTaoTH
                 //    res = res.Where(x => x.GVID == id).ToList();
                 //}
 
-                ViewBag.PhuongPhapDT_ID = PhuongPhapDT_ID;
-                ViewBag.PhanLoaiNCDT_ID = PhanLoaiNCDT_ID;
+                ViewBag.PhuongPhapDT_ID = Convert.ToInt32(Request.QueryString["PhuongPhapDT_ID"]);
+                ViewBag.PhanLoaiNCDT_ID = Convert.ToInt32(Request.QueryString["PhanLoaiNCDT_ID"]);
 
 
                 List<PhongBan> dt = db_context.PhongBans.ToList();
@@ -176,17 +177,8 @@ namespace E_Learning.Controllers.DaoTaoTH
 
             try
             {
-                //foreach (var key in ModelState.Keys)
-                //{
-                //    foreach (var error in ModelState[key].Errors)
-                //    {
-                //        Console.WriteLine($"Lỗi tại {key}: {error.ErrorMessage}");
-                //    }
-                //}
                 if (ModelState.IsValid)
                 {
-                    string filePathSave = null;
-                    int countSl = 0;
                     // Thêm lớp học
                     var ncdt = db_context.SH_NhuCauDT.Where(x => x.ID == tochucdt.NCDT_ID).FirstOrDefault();
                     var nam = db_context.SH_QuyDaoTao.First().AD_Nam;
@@ -379,7 +371,7 @@ namespace E_Learning.Controllers.DaoTaoTH
                         lophoc.TinhTrang = 0; // Đang lưu
                         db_context.SaveChanges();
                     }
-
+                    
                     TempData["msgSuccess"] = "<script>alert('Thêm mới thành công ');</script>";
                     return RedirectToAction("Index");
                 }
@@ -548,6 +540,282 @@ namespace E_Learning.Controllers.DaoTaoTH
             {
                 return Json(new { success = false, message = "Lỗi xử lý file: " + ex.Message });
             }
+        }
+
+        public ActionResult Edit(int? PhuongPhapDT_ID, int? PhanLoaiNCDT_ID, int? IDLH)
+        {
+            var ListQuyen = new HomeController().GetPermisionCN(Idquyen, ControllerName);
+
+            var res = db_context.LopHocs.Find(IDLH);
+            List<NhuCauDTTHView> ncdtData = (from a in db_context.SH_NhuCauDT.Where(x => (PhanLoaiNCDT_ID == null || x.PhanLoaiNCDT_ID == PhanLoaiNCDT_ID) 
+                                             && (PhuongPhapDT_ID == null || x.PhuongPhapDT_ID == PhuongPhapDT_ID) && x.BoPhanLNC_ID == MyAuthentication.IDPhongban)
+                                   select new NhuCauDTTHView
+                                   {
+                                       ID_NCDT = a.ID,
+                                       TenNoiDungDT = "Mã NCĐT: " + a.ID + " - " + a.NoiDungDT.NoiDung
+                                   }).ToList();
+
+            var data = new ManageClassValidation();
+            if (res != null)
+            {
+                data.MaLH = res.MaLH;
+                data.TenLH = res.TenLH;
+                data.NoiDungTrichYeu = res.NoiDungTrichYeu;
+                data.TGBDLH = res.TGBDLH.Value;
+                data.TGKTLH = res.TGKTLH.Value;
+                data.DiaDiemDT = res.DiaDiemDT;
+                data.ThoiLuongDT = res.ThoiLuongDT;
+                data.NCDT_ID = (int) res.NCDT_ID;
+                data.QuyDT = (int) res.QuyDT;
+                data.IDDeThi = (int) res.IDDeThi;
+                data.NguoiKiemTra_ID = res.NguoiKiemTra_ID;
+                data.IDLH = res.IDLH;
+            }
+
+            ViewBag.NCDT_DATA = new SelectList(ncdtData, "ID_NCDT", "TenNoiDungDT", data.NCDT_ID);
+
+            var listDeThi = new List<CauHoiDeThiCTDTTH>();
+
+            if (data.NCDT_ID != null)
+            {
+                var ncdt = db_context.SH_NhuCauDT.FirstOrDefault(x => x.ID == data.NCDT_ID);
+
+                if (ncdt != null)
+                {
+                    listDeThi = (from x in db_context.DeThis
+                                 where x.IDND == ncdt.NoiDungDT_ID
+                                 select new CauHoiDeThiCTDTTH
+                                 {
+                                     ID = x.IDDeThi,
+                                     TenDeThi = string.Concat("Điểm chuẩn: ", x.DiemChuan, "-", x.TenDe),
+                                 }).ToList();
+                }
+            }
+
+            ViewBag.DETHI_DATA = new SelectList(listDeThi, "ID", "TenDeThi", data.IDDeThi);
+
+            var listNhanVien = db_context.NhanViens.Where(x => x.IDTinhTrangLV == 1)
+                .Select(x => new EmployeeValidation { ID = x.ID, HoTen = x.MaNV + " - " + x.HoTen, IDPhongBan = (int)x.IDPhongBan })
+                .ToList();
+
+            int IDGVCty = db_context.SH_ChiTietTCDT.Where(x => x.CTDT_ID == data.IDLH).SingleOrDefault().ID_GVCty.Value;
+
+            ViewBag.ID_NhanVien = new SelectList(listNhanVien, "ID", "HoTen", IDGVCty);
+            ViewBag.ID_NguoiKiemTra = new SelectList(listNhanVien.Where(x => x.IDPhongBan == MyAuthentication.IDPhongban), "ID", "HoTen", data.NguoiKiemTra_ID);
+            ViewBag.PhuongPhapDT_ID = PhuongPhapDT_ID;
+            ViewBag.PhanLoaiNCDT_ID = PhanLoaiNCDT_ID;
+            ViewBag.LoaiNCDT = PhanLoaiNCDT_ID;
+            ViewBag.LoaiHinh_DT = db_context.SH_PhanLoaiNCDT.Where(x => x.IDLoai == PhanLoaiNCDT_ID).FirstOrDefault().LoaiHinhDT_ID;
+            
+            var IDPB = MyAuthentication.IDPhongban;
+            ViewBag.Nam = db_context.SH_QuyDaoTao.First().AD_Nam;
+            ViewBag.Quy = db_context.SH_QuyDaoTao.First().AD_Quy;
+            ViewBag.BoPhan_ID = new SelectList(db_context.PhongBans.Where(x => x.IDPhongBan == IDPB), "IDPhongBan", "TenPhongBan", IDPB);
+            ViewBag.MaLH = data.MaLH;
+            
+            return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ManageClassValidation DTO, HttpPostedFileBase fileUpload, string action, int? PhuongPhapDT_ID, int? PhanLoaiNCDT_ID)
+        {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    var lopHocExisting = db_context.LopHocs.Where(x => x.MaLH == DTO.MaLH).FirstOrDefault();
+                    if (lopHocExisting != null)
+                    {
+                        lopHocExisting.TenLH = DTO.TenLH;
+                        lopHocExisting.NCDT_ID = DTO.NCDT_ID;
+                        lopHocExisting.NoiDungTrichYeu = DTO.NoiDungTrichYeu;
+                        lopHocExisting.TGBDLH = DTO.TGBDLH;
+                        lopHocExisting.TGKTLH = DTO.TGKTLH;
+                        lopHocExisting.ThoiLuongDT = DTO.ThoiLuongDT;
+                        lopHocExisting.DiaDiemDT = DTO.DiaDiemDT;
+                        var nhuCauDT = db_context.SH_NhuCauDT.Where(x => x.ID == lopHocExisting.NCDT_ID).FirstOrDefault();
+                        lopHocExisting.NDID = nhuCauDT.NoiDungDT_ID;
+
+                        // int IDGVCty = db_context.SH_ChiTietTCDT.Where(x => x.CTDT_ID == lopHocExisting.IDLH).SingleOrDefault().ID_GVCty.Value;
+                        int IDGVCty = DTO.chiTietToChucDTTH.ID_GVCty.Value;
+                        lopHocExisting.GVID = IDGVCty;
+                        var chiTietTCDTInDB = db_context.SH_ChiTietTCDT.Where(x => x.CTDT_ID == lopHocExisting.IDLH).SingleOrDefault();
+                        var nhanVienInDb = db_context.NhanViens.Where(x => x.ID == IDGVCty).SingleOrDefault();
+
+                        chiTietTCDTInDB.ID_GVCty = IDGVCty;
+                        chiTietTCDTInDB.MaNV_GV = nhanVienInDb.MaNV;
+                        chiTietTCDTInDB.HoTenGV = nhanVienInDb.HoTen;
+                        var viTriCongViecInDb = db_context.Vitris.Where(x => x.IDViTri == nhanVienInDb.IDViTri).SingleOrDefault();
+                        chiTietTCDTInDB.ViTriCV_GV = viTriCongViecInDb.TenViTri;
+                        var phongBanInDb = db_context.PhongBans.Where(x => x.IDPhongBan == nhanVienInDb.IDPhongBan).SingleOrDefault();
+                        chiTietTCDTInDB.DonVi_GV = phongBanInDb.TenPhongBan;
+
+                        lopHocExisting.NguoiKiemTra_ID = DTO.NguoiKiemTra_ID;
+                        lopHocExisting.NguoiTao_ID = lopHocExisting.NguoiTao_ID;
+                        lopHocExisting.NgayTao = lopHocExisting.NgayTao;
+                        lopHocExisting.IDDeThi = DTO.IDDeThi;
+                    }
+
+                    
+                    //// thêm học viên vào lớp
+                    //if (DTO.IsAll)
+                    //{
+                    //    var dsvt = db_context.SH_ViTri_NDDT.Where(x => x.NCDT_ID == DTO.NCDT_ID).ToList();
+                    //    foreach (var item in dsvt)
+                    //    {
+                    //        var nhanvien = db_context.NhanViens.Where(x => x.IDVTKNL == item.Vitri_ID && x.IDTinhTrangLV == 1).ToList();
+                    //        foreach (var item1 in nhanvien)
+                    //        {
+                    //            var checknv = db_context.XNHocTaps.Where(x => x.NVID == item1.ID && x.LHID == lophoc.IDLH).FirstOrDefault();
+                    //            if (checknv == null)
+                    //            {
+                    //                var XNHT = new XNHocTap()
+                    //                {
+                    //                    LHID = lophoc.IDLH,
+                    //                    NVID = item1.ID,
+                    //                    PBID = item1.IDPhongBan,
+                    //                    VTID = item1.IDViTri,
+                    //                    NgayTG = default,
+                    //                    NgayHT = default,
+                    //                    XNHT = false,
+                    //                    XNTG = false
+                    //                };
+                    //                db_context.XNHocTaps.Add(XNHT);
+                    //                db_context.SaveChanges();
+                    //            }
+                    //        }
+                    //    }
+                    //    var dsnv = db_context.SH_KetQuaDaoTao.Where(x => x.NCDT_ID == DTO.NCDT_ID).ToList();
+                    //    foreach (var item in dsnv)
+                    //    {
+                    //        var nhanvien = db_context.NhanViens.Where(x => x.ID == item.NhanVien_ID && x.IDTinhTrangLV == 1).FirstOrDefault();
+                    //        if (nhanvien != null)
+                    //        {
+                    //            var checknv = db_context.XNHocTaps.Where(x => x.NVID == nhanvien.ID && x.LHID == lophoc.IDLH).FirstOrDefault();
+                    //            if (checknv == null)
+                    //            {
+                    //                var XNHT = new XNHocTap()
+                    //                {
+                    //                    LHID = lophoc.IDLH,
+                    //                    NVID = nhanvien.ID,
+                    //                    PBID = nhanvien.IDPhongBan,
+                    //                    VTID = nhanvien.IDViTri,
+                    //                    NgayTG = default(DateTime),
+                    //                    NgayHT = default(DateTime),
+                    //                    XNHT = false,
+                    //                    XNTG = false
+                    //                };
+                    //                db_context.XNHocTaps.Add(XNHT);
+                    //                db_context.SaveChanges();
+                    //            }
+                    //        }
+
+                    //    }
+                    //}
+                    //// check ds học viên có học viên để mở lớp
+                    //if (!String.IsNullOrEmpty(DTO.DSHocVien))
+                    //{
+                    //    //Regex.Replace(_DO.NVDG, @"[^0-9a-zA-Z]+", " ");
+                    //    string tx = Regex.Replace(DTO.DSHocVien, @"[^0-9a-zA-Z]+", " ");
+                    //    string[] NVS = tx.Split(new char[] { ' ' });
+                    //    foreach (var item in NVS)
+                    //    {
+                    //        var aa = db_context.NhanViens.Where(x => x.MaNV == item).FirstOrDefault();
+                    //        if (aa != null)
+                    //        {
+                    //            var checktrung = db_context.XNHocTaps.Where(x => x.NVID == aa.ID && x.LHID == lophoc.IDLH).ToList();
+                    //            if (checktrung.Count() == 0)
+                    //            {
+                    //                var XNHT = new XNHocTap()
+                    //                {
+                    //                    LHID = lophoc.IDLH,
+                    //                    NVID = aa.ID,
+                    //                    PBID = aa.IDPhongBan,
+                    //                    VTID = aa.IDViTri,
+                    //                    NgayTG = default(DateTime),
+                    //                    NgayHT = default(DateTime),
+                    //                    XNHT = false,
+                    //                    XNTG = false
+                    //                };
+                    //                db_context.XNHocTaps.Add(XNHT);
+                    //                db_context.SaveChanges();
+                    //            }
+                    //        }
+                    //    }
+                    //    db_context.SaveChanges();
+                    //}
+                    //// thêm học viên từ DS excel
+                    //if (fileUpload != null && fileUpload.ContentLength > 0)
+                    //{
+                    //    //lưu câu hỏi
+                    //    Stream stream = fileUpload.InputStream;
+
+                    //    IExcelDataReader reader = null;
+                    //    if (fileUpload.FileName.EndsWith(".xls"))
+                    //    {
+                    //        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                    //    }
+                    //    else if (fileUpload.FileName.EndsWith(".xlsx"))
+                    //    {
+                    //        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    //    }
+                    //    else
+                    //    {
+                    //        TempData["msg"] = "<script>alert('Vui lòng chọn đúng định dạng file Excel');</script>";
+                    //        RedirectToAction("Index");
+                    //    }
+                    //    DataSet result = reader.AsDataSet();
+                    //    DataTable dt = result.Tables[0];
+                    //    reader.Close();
+
+                    //    for (int i = 1; i < dt.Rows.Count; i++)
+                    //    {
+                    //        string manv = dt.Rows[i][1].ToString().Trim();
+                    //        var nv = db_context.NhanViens.Where(x => x.MaNV == manv).FirstOrDefault();
+                    //        if (nv != null)
+                    //        {
+                    //            var checktrung = db_context.XNHocTaps.Where(x => x.NVID == nv.ID && x.LHID == lophoc.IDLH).ToList();
+                    //            if (checktrung.Count() == 0)
+                    //            {
+                    //                var XNHT = new XNHocTap()
+                    //                {
+                    //                    LHID = lophoc.IDLH,
+                    //                    NVID = nv.ID,
+                    //                    PBID = nv.IDPhongBan,
+                    //                    VTID = nv.IDViTri,
+                    //                    NgayTG = default(DateTime),
+                    //                    NgayHT = default(DateTime),
+                    //                    XNHT = false,
+                    //                    XNTG = false
+                    //                };
+                    //                db_context.XNHocTaps.Add(XNHT);
+                    //                db_context.SaveChanges();
+                    //            }
+                    //        }
+                    //    }
+                    //}
+
+                    // Thêm thông tin trình ký
+                    if (action == "Cập nhật")
+                    {
+                        lopHocExisting.TinhTrang = 0; // Đang lưu
+                        db_context.SaveChanges();
+                    }
+
+                    TempData["msgSuccess"] = "<script>alert('Cập nhật thành công ');</script>";
+                    
+                    return RedirectToAction("Index", "ToChucDTTH");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["msgSuccess"] = "<script>alert('Cập nhật thất bại " + ex.Message + " ');</script>";
+            }
+            
+            return RedirectToAction("Index", "ToChucDTTH");
         }
 
     }
