daff488f2710e6687069bf7be6f92284ad15f876
HPDQ\nguyenvotuannhan <nguyenvotuannhan@hoaphat.com.vn>
2025-03-06 10:24:21 +0700

    add view honor

---
+++ E-Learning/Controllers/KhenThuong/HonorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Learning.Controllers.KhenThuong
+{
+    public class HonorController : Controller
+    {
+        // GET: Honor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
