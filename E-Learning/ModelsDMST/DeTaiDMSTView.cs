0d96ba1fc5647bf3d12e70131c746c60ad3a9af7
HPDQ\nguyenvotuannhan <nguyenvotuannhan@hoaphat.com.vn>
2025-06-30 07:52:29 +0700

    update view model

---
+++ E-Learning/ModelsDMST/DeTaiDMSTView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace E_Learning.ModelsDMST
+{
+    public class DeTaiDMSTView
+    {
+        public int ID { get; set; }
+        public string TenYTuong { get; set; }
+        public string NoiDungYTuong { get; set; }
+        public string ViTriTrienKhai { get; set; }
+        public string HieuQuaKyVong { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+        public int ID_NhanVienDaiDien { get; set; }
+    }
+}
