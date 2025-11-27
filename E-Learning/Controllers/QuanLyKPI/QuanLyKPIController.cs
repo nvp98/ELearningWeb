d3fd8a0994511bfd31f8eda86c3285c9abd21ecf
HPDQ\nguyenvotuannhan <nguyenvotuannhan@hoaphat.com.vn>
2025-11-27 11:16:42 +0700

    add view

---
+++ E-Learning/Controllers/QuanLyKPI/QuanLyKPIController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Learning.Controllers.QuanLyKPI
+{
+    public class QuanLyKPIController : Controller
+    {
+        // GET: QuanLyKPI
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult BangMoTaKPI()
+        {
+            return View();
+        }
+
+        public ActionResult NhapLieuKPI()
+        {
+            return View();
+        }
+    }
+}
